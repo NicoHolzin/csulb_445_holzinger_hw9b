3b05d53b4c7c44d0f22be1037bea97c638696da1
Nicolas Holzinger <nicoholzin@gmail.com>
2024-11-07 00:14:09 -0800

    v2

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const express = require("express");
 
-
 const app = express();
 const PORT = 3000;
 
-// Middleware pour parser les données JSON
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-
-// Route principale 
 app.get("/", (req, res) => {
   res.sendFile('index.html', { root: __dirname + '/views' });
 });
@@ -40,7 +36,6 @@ app.post("/submit", (req, res) => {
   `);
 });
 
-// Route principale qui sert hex1.html
 app.get("/ex2", (req, res) => {
   res.sendFile('ex2.html', { root: __dirname + '/views' });
 });
@@ -48,14 +43,27 @@ app.get("/ex2", (req, res) => {
 app.post("/api/countries", (req, res) => {
     const { name, countries } = req.body;
 
-    // Calcul du nombre de pays visités
     const numCountries = countries ? countries.length : 0;
     const message = `Votre nom est ${name} et vous avez visité ${numCountries} pays. Continuez à voyager !`;
 
-    // Retourne le message en JSON
     res.json({ message });
 });
 
+let articles = [];
+app.post("/articles", (req, res) => {
+  const { title, content } = req.body;
+
+  // Determine the next article ID
+  const maxId = articles.length > 0 ? Math.max(...articles.map(article => article.id)) : 0;
+  const newId = maxId + 1;
+
+  // Create and store the new article
+  const newArticle = { id: newId, title, content };
+  articles.push(newArticle);
+
+  // Send a response with the article title and ID
+  res.send(`New article added successfully with title "${title}" and ID ${newId}!`);
+});
 
 app.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
